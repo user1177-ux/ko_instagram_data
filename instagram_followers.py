844acc3387c5f9cbf6ababc6a83654cdcfc3b688
user1177-ux <morozova.s.m.w@gmail.com>
2024-08-16 15:06:45 +0400

    Create instagram_followers.py

---
+++ instagram_followers.py
@@ -0,0 +1,41 @@
+import requests
+import datetime
+import csv
+
+# Ваш токен доступа
+access_token = 'ACCESS_TOKEN'
+# ID вашего бизнес-аккаунта Instagram
+instagram_account_id = 'INSTAGRAM_ACCOUNT_ID'
+
+# Конечная точка API для получения количества подписчиков
+url = f'https://graph.facebook.com/v12.0/{instagram_account_id}/insights'
+
+# Запрашиваемые параметры
+params = {
+    'metric': 'follower_count',
+    'period': 'day',
+    'access_token': access_token
+}
+
+# Получаем текущую дату и дату два месяца назад
+end_date = datetime.datetime.now().date()
+start_date = (end_date - datetime.timedelta(days=60)).strftime('%Y-%m-%d')
+
+# Добавляем временной интервал в параметры запроса
+params['since'] = start_date
+params['until'] = end_date.strftime('%Y-%m-%d')
+
+# Отправляем запрос к API
+response = requests.get(url, params=params)
+data = response.json()
+
+# Сохраняем данные в CSV файл
+with open('instagram_followers.csv', 'w', newline='') as csvfile:
+    fieldnames = ['date', 'followers_count']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    
+    for entry in data['data'][0]['values']:
+        writer.writerow({'date': entry['end_time'], 'followers_count': entry['value']})
+
+print("Данные успешно выгружены в файл instagram_followers.csv")
