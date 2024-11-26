37f0aa013ec5e7d1e0fb07de97c7165c97aed732
user1177-ux <morozova.s.m.w@gmail.com>
2024-11-26 14:53:46 +0400

    Update instagram_posts_data.py

--- instagram_posts_data.py
+++ instagram_posts_data.py
@@ -15,12 +15,18 @@ base_url = f'https://graph.facebook.com/v20.0/{instagram_account_id}/media?field
 
 # Словарь для хранения публикаций, лайков и комментариев по датам
 posts_data = {}
+earliest_date = None  # Переменная для самой ранней даты публикации
 
 # Функция для обработки данных со страницы
 def process_page(data):
+    global earliest_date
     for media in data.get('data', []):
         media_id = media['id']
         date_str = media['timestamp'][:10]  # Извлекаем дату (YYYY-MM-DD)
+
+        # Определяем самую раннюю дату
+        if earliest_date is None or date_str < earliest_date:
+            earliest_date = date_str
 
         # Запрашиваем лайки и комментарии для конкретного поста
         metrics_url = f'https://graph.facebook.com/v20.0/{media_id}?fields=like_count,comments_count&access_token={access_token}'
@@ -50,22 +56,25 @@ while next_url:
     # Переходим на следующую страницу, если она есть
     next_url = data.get('paging', {}).get('next')
 
-# Создаём полный список дат за год
-start_date = datetime(datetime.now().year, 1, 1)
-end_date = datetime(datetime.now().year, 12, 31)
-all_dates = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range((end_date - start_date).days + 1)]
+# Создаём полный список дат начиная с earliest_date до сегодняшнего дня
+if earliest_date:
+    start_date = datetime.strptime(earliest_date, '%Y-%m-%d')
+    end_date = datetime.now()
+    all_dates = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range((end_date - start_date).days + 1)]
 
-# Убедимся, что для всех дат есть данные (добавляем 0 для отсутствующих дат)
-for date in all_dates:
-    if date not in posts_data:
-        posts_data[date] = {'posts_count': 0, 'likes': 0, 'comments': 0}
+    # Убедимся, что для всех дат есть данные (добавляем 0 для отсутствующих дат)
+    for date in all_dates:
+        if date not in posts_data:
+            posts_data[date] = {'posts_count': 0, 'likes': 0, 'comments': 0}
 
-# Сохраняем результаты в CSV файл
-with open('instagram_posts_data.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Дата', 'Количество публикаций', 'Лайки', 'Комментарии'])
-    for date in sorted(posts_data.keys()):
-        metrics = posts_data[date]
-        writer.writerow([date, metrics['posts_count'], metrics['likes'], metrics['comments']])
+    # Сохраняем результаты в CSV файл
+    with open('instagram_posts_data.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Дата', 'Количество публикаций', 'Лайки', 'Комментарии'])
+        for date in sorted(posts_data.keys()):
+            metrics = posts_data[date]
+            writer.writerow([date, metrics['posts_count'], metrics['likes'], metrics['comments']])
 
-print("Данные успешно сохранены в 'instagram_posts_data.csv'.")
+    print(f"Данные успешно сохранены в 'instagram_posts_data.csv'. Данные с {earliest_date} по {end_date.strftime('%Y-%m-%d')}.")
+else:
+    print("Нет данных для записи в файл.")
