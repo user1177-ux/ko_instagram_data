41925e3e62ee9e3416f88fc887c6a827c7374220
user1177-ux <morozova.s.m.w@gmail.com>
2024-11-29 10:49:15 +0400

    Update instagram_posts_data.py

--- instagram_posts_data.py
+++ instagram_posts_data.py
@@ -7,7 +7,7 @@ access_token = 'EAAUjRaaBokMBOwfYtk6tknzsZCkDEy0qZAZBIdOzZA6V3uZCJyZBBRONZCEpzBb
 instagram_account_id = '17841459665084773'
 
 # URL для запроса первой страницы медиа-объектов
-base_url = f'https://graph.facebook.com/v20.0/{instagram_account_id}/media?fields=id,media_type,timestamp,like_count,comments_count,saved_count,shared_count,insights.metric(impressions)&access_token={access_token}'
+base_url = f'https://graph.facebook.com/v20.0/{instagram_account_id}/media?fields=id,media_type,timestamp,like_count,comments_count,permalink,insights.metric(impressions,reach)&access_token={access_token}'
 
 # Словарь для хранения данных
 posts_data = {}
@@ -23,16 +23,39 @@ def transform_media_type(media_type):
         return 'карусель'
     return 'неизвестно'
 
-# Функция для генерации ссылки на пост
-def generate_post_link(media_id):
-    return f"https://www.instagram.com/p/{media_id}/"
+# Функция для получения дополнительных метрик рилсов
+def get_reels_metrics(media_id):
+    url = f'https://graph.facebook.com/v20.0/{media_id}/insights?metric=plays,video_views,ig_reels_aggregated_all_plays_count,ig_reels_avg_watch_time&period=lifetime&access_token={access_token}'
+    response = requests.get(url)
+    data = response.json()
+
+    # Лог для проверки данных
+    print(f"Ответ метрик для рилса {media_id}: {data}")
+
+    metrics = {
+        'plays': None,
+        'video_views': None,
+        'all_plays_count': None,
+        'avg_watch_time': None
+    }
+
+    # Извлекаем значения метрик
+    for item in data.get('data', []):
+        if item['name'] == 'plays':
+            metrics['plays'] = item['values'][0]['value']
+        elif item['name'] == 'video_views':
+            metrics['video_views'] = item['values'][0]['value']
+        elif item['name'] == 'ig_reels_aggregated_all_plays_count':
+            metrics['all_plays_count'] = item['values'][0]['value']
+        elif item['name'] == 'ig_reels_avg_watch_time':
+            metrics['avg_watch_time'] = item['values'][0]['value']
+
+    return metrics
 
 # Функция для обработки данных со страницы
 def process_page(data):
     global earliest_date
     for media in data.get('data', []):
-        print("Медиа объект:", media)  # Отладочный вывод для проверки данных
-
         media_id = media['id']
         media_type = transform_media_type(media.get('media_type', ''))  # Преобразуем тип
         date_str = media['timestamp'][:10]  # Извлекаем дату (YYYY-MM-DD)
@@ -45,22 +68,26 @@ def process_page(data):
         if date_str not in posts_data:
             posts_data[date_str] = []
 
-        # Получаем метрики из объекта
-        views = 0
-        insights = media.get('insights', {}).get('data', [])
-        for metric in insights:
-            if metric['name'] == 'impressions':
-                views = metric.get('values', [{}])[0].get('value', 0)
+        # Получаем базовые метрики
+        likes = media.get('like_count', 0)
+        comments = media.get('comments_count', 0)
+        permalink = media.get('permalink', '')
+
+        # Если это рилс, запрашиваем дополнительные метрики
+        reels_metrics = {}
+        if media_type == 'рилс':
+            reels_metrics = get_reels_metrics(media_id)
 
         # Добавляем данные по посту
         posts_data[date_str].append({
             'media_type': media_type,
-            'views': views,
-            'likes': media.get('like_count', 0),
-            'comments': media.get('comments_count', 0),
-            'shares': media.get('shared_count', 0),
-            'saves': media.get('saved_count', 0),
-            'link': generate_post_link(media_id)
+            'likes': likes,
+            'comments': comments,
+            'permalink': permalink,
+            'plays': reels_metrics.get('plays'),
+            'video_views': reels_metrics.get('video_views'),
+            'all_plays_count': reels_metrics.get('all_plays_count'),
+            'avg_watch_time': reels_metrics.get('avg_watch_time')
         })
 
 # Обрабатываем все страницы
@@ -69,7 +96,8 @@ while next_url:
     response = requests.get(next_url)
     data = response.json()
 
-    print("Ответ API для страницы:", data)  # Лог для проверки страницы
+    # Лог для проверки текущей страницы
+    print("Ответ API для страницы:", data)
 
     # Обрабатываем текущую страницу данных
     process_page(data)
@@ -89,13 +117,22 @@ if earliest_date:
             posts_data[date] = []
 
     # Сохраняем результаты в CSV файл
-    print("Итоговые данные для CSV:", posts_data)  # Лог перед записью в файл
     with open('instagram_posts_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Дата', 'Тип поста', 'Просмотры', 'Лайки', 'Комментарии', 'Репосты', 'Сохранения', 'Ссылка'])
+        writer.writerow(['Дата', 'Тип поста', 'Лайки', 'Комментарии', 'Ссылка', 'Первичные воспроизведения', 'Просмотры (3 секунды)', 'Всего воспроизведений', 'Среднее время просмотра (мс)'])
         for date in sorted(posts_data.keys()):
             for post in posts_data[date]:
-                writer.writerow([date, post['media_type'], post['views'], post['likes'], post['comments'], post['shares'], post['saves'], post['link']])
+                writer.writerow([
+                    date,
+                    post['media_type'],
+                    post['likes'],
+                    post['comments'],
+                    post['permalink'],
+                    post.get('plays'),
+                    post.get('video_views'),
+                    post.get('all_plays_count'),
+                    post.get('avg_watch_time')
+                ])
 
     print(f"Данные успешно сохранены в 'instagram_posts_data.csv'. Данные с {earliest_date} по {end_date.strftime('%Y-%m-%d')}.")
 else:
